1d2aa33e145d3c75b792512d2af3248b7da8377b
Maksym D <max.moderation@gmail.com>
2023-07-14 13:12:42 +0200

    feat: create is palindrom file

---
+++ isPalindrome.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+const isPalindrome = function (x) {
+  const arrayX = x.toString().split("");
+
+  for (let head = 0, tail = arrayX.length - 1; head < arrayX.length; head++) {
+    if (arrayX[head] === arrayX[tail]) {
+      tail--;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+console.log(isPalindrome(1212));
